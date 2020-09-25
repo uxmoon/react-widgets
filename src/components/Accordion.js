ca03c4246b0ec930931dfc43597c100621d774de
Martin Luna <martin.luna@possible.com>
2020-09-25 10:44:16 -0300

    introducing useState, is a hook that give access to State inside fn comps

--- src/components/Accordion.js
+++ src/components/Accordion.js
@@ -1,10 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Accordion = ({ items }) => {
+  /* useState is a hook, gives us access to State inside of a fn component */
+  const [activeIndex, setActiveIndex] = useState(null);
 
   /* Helper function */
   const onTitleClick = (index) => {
-    console.log("title clicked", index);
+    // console.log("title clicked", index);
+
+    /* update State */
+    setActiveIndex(index);
   };
 
   const renderedItems = items.map((item, index) => {
@@ -12,8 +17,7 @@ const Accordion = ({ items }) => {
       <React.Fragment key={item.title}>
         <div
           className="title active"
-          onClick={() => onTitleClick(index)}
-        >
+          onClick={() => onTitleClick(index)}>
           <i className="dropdown icon"></i>
           {item.title}
         </div>
@@ -24,7 +28,12 @@ const Accordion = ({ items }) => {
     );
   });
 
-  return <div className="ui styled accordion">{renderedItems}</div>;
+  return (
+    <div className="ui styled accordion">
+      {renderedItems}
+      <h1>{activeIndex}</h1>
+    </div>
+  );
 };
 
 export default Accordion;
