4b62389f126760cef3d8d7cb654f6d79f4191baf
Martin Luna <martin.luna@possible.com>
2020-09-25 17:09:31 -0300

    implementing a delayed request with a cleanup function inside useEffect

--- src/components/Search.js
+++ src/components/Search.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import axios from "axios";
 
 const Search = () => {
-  const [term, setTerm] = useState("");
+  const [term, setTerm] = useState("Programming");
   const [results, setResults] = useState([]);
 
   /* test results */
@@ -10,33 +10,32 @@ const Search = () => {
 
   /* make a request inside of useEffect with axios using async code */
   useEffect(() => {
-    // const search = async () => {
-    //   const { data } = await axios.get("https://en.wikipedia.org/w/api.php", {
-    //     params: {
-    //       action: "query",
-    //       list: "search",
-    //       origin: "*",
-    //       format: "json",
-    //       srsearch: term,
-    //     },
-    //   });
+    const search = async () => {
+      const { data } = await axios.get("https://en.wikipedia.org/w/api.php", {
+        params: {
+          action: "query",
+          list: "search",
+          origin: "*",
+          format: "json",
+          srsearch: term,
+        },
+      });
 
-    //   /* update results State */
-    //   setResults(data.query.search);
-    // };
+      /* update results State */
+      setResults(data.query.search);
+    };
 
-    // /* reduce API requests */
-    // const timeoutId = setTimeout(() => {
-    //   /* don't search if a term is not provided */
-    //   if (term) {
-    //     search();
-    //   }
-    // }, 500);
-    console.log("init term or term changed")
+    /* reduce API requests */
+    const timeoutId = setTimeout(() => {
+      /* don't search if a term is not provided */
+      if (term) {
+        search();
+      }
+    }, 800);
 
     /* clean up function, react will call this fn in a certain point in time */
     return () => {
-      console.log("clean up")
+      clearTimeout(timeoutId);
     };
   }, [term]);
 
