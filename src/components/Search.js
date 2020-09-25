f1dc4cefd10183c44094209618252293ecbc4863
Martin Luna <martin.luna@possible.com>
2020-09-25 14:57:18 -0300

    add http req to wikipedia api

--- src/components/Search.js
+++ src/components/Search.js
@@ -4,10 +4,19 @@ import axios from "axios";
 const Search = () => {
   const [term, setTerm] = useState("");
 
+  /* make a request inside of useEffect with axios */
   useEffect(() => {
 
     const search = async () => {
-      await axios.get("https://")
+      await axios.get("https://en.wikipedia.org/w/api.php", {
+        params: {
+          action: "query",
+          list: "search",
+          origin: "*",
+          format: "json",
+          srsearch: term,
+        }
+      })
     };
 
     search();
