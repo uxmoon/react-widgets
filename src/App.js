443c6474b1d676c6f80296533e19ebf59f39297a
Martin Luna <martin.luna@possible.com>
2020-09-25 17:41:35 -0300

    Dropdown, added new component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Accordion from "./components/Accordion";
 import Search from "./components/Search";
+import Dropdown from "./components/Dropdown";
 
 /* static array */
 const items = [
@@ -21,7 +22,7 @@ const items = [
 export default () => {
   return (
     <div>
-      <Search />
+      <Dropdown />
     </div>
   );
 };
