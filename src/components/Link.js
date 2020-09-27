dba728ac3749d0204b31b1234d787f5496101290
Martin Luna <uxmoon@gmail.com>
2020-09-27 19:51:05 -0300

    Emmit event navigation, Detecting navigation with addEventListener popstate

--- src/components/Link.js
+++ src/components/Link.js
@@ -2,10 +2,15 @@ import React from 'react';
 
 const Link = ({ className, href, children }) => {
   const onClick = (event) => {
+    /* prevent page refresh on click */
     event.preventDefault();
 
     /* update url without page refresh */
     window.history.pushState({}, '', href);
+
+    /* Emmit a navigation event to the Route components */
+    const navEvent = new PopStateEvent('popstate');
+    window.dispatchEvent(navEvent);
   };
   return (
     <a onClick={onClick} className={className} href={href}>
--- src/components/Route.js
+++ src/components/Route.js
@@ -1,12 +1,23 @@
+import { useEffect } from 'react';
 /**
  * No need to import React when not using JSX
  * @param {path} window.location.pathname
  * children is the Component to show
  */
 const Route = ({ path, children }) => {
-  return window.location.pathname === path
-    ? children
-    : null;
+  useEffect(() => {
+    const onLocationChange = () => {
+      console.log('location changed');
+    };
+    window.addEventListener('popstate', onLocationChange);
+
+    return () => {
+      window.removeEventListener('popstate', onLocationChange);
+    };
+  }, []);
+  return window.location.pathname === path ? children : null;
 };
+
+/* Listen to navigation event */
 
 export default Route;
