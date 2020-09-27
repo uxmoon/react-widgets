0c6ac8d7e282c79f6cb4dbe0d4cc14fb5f68f13e
Martin Luna <uxmoon@gmail.com>
2020-09-27 19:42:29 -0300

    changing url without page refresh

--- src/components/Link.js
+++ src/components/Link.js
@@ -3,6 +3,9 @@ import React from 'react';
 const Link = ({ className, href, children }) => {
   const onClick = (event) => {
     event.preventDefault();
+
+    /* update url without page refresh */
+    window.history.pushState({}, '', href);
   };
   return (
     <a onClick={onClick} className={className} href={href}>
