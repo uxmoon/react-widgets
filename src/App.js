a32d61e403d14079a3a1442d2ebb2b3085fda11c
Martin Luna <uxmoon@gmail.com>
2020-09-27 18:46:36 -0300

    added basic component routing

--- src/App.js
+++ src/App.js
@@ -1,44 +1,71 @@
-import React, { useState } from "react";
-import Accordion from "./components/Accordion";
-import Search from "./components/Search";
-import Dropdown from "./components/Dropdown";
+import React, { useState } from 'react';
+import Accordion from './components/Accordion';
+import Search from './components/Search';
+import Dropdown from './components/Dropdown';
 import Translate from './components/Translate';
 
 /* static array */
 const items = [
   {
-    title: "What is React?",
-    content: "React is a front end javascript framework.",
+    title: 'What is React?',
+    content: 'React is a front end javascript framework.',
   },
   {
-    title: "Why us React?",
-    content: "React is a favorite JS library among engineers.",
+    title: 'Why us React?',
+    content: 'React is a favorite JS library among engineers.',
   },
   {
-    title: "How do you use React?",
-    content: "You use React by creating components.",
+    title: 'How do you use React?',
+    content: 'You use React by creating components.',
   },
 ];
 
 const options = [
   {
-    label: "The color red",
-    value: "red",
+    label: 'The color red',
+    value: 'red',
   },
   {
-    label: "The color green",
-    value: "green",
+    label: 'The color green',
+    value: 'green',
   },
   {
-    label: "The color blue",
-    value: "blue",
+    label: 'The color blue',
+    value: 'blue',
   },
 ];
+
+const showAccordion = () => {
+  if (window.location.pathname === '/') {
+    return <Accordion items={items} />;
+  }
+};
+
+const showList = () => {
+  if (window.location.pathname === '/list') {
+    return <Search />;
+  }
+};
+
+const showDropdown = () => {
+  if (window.location.pathname === '/dropdown') {
+    return <Dropdown />;
+  }
+};
+
+const showTranslate = () => {
+  if (window.location.pathname === '/translate') {
+    return <Translate />;
+  }
+};
 
 export default () => {
   return (
     <div className="ui container">
-      <Translate />
+      {showAccordion()}
+      {showList()}
+      {showDropdown()}
+      {showTranslate()}
     </div>
   );
 };
