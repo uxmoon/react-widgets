11496907f6a3785ce70a2142780d1e806d69e3a7
Martin Luna <uxmoon@gmail.com>
2020-09-27 12:39:16 -0300

    added axios to make a request to Google Translate API

--- src/components/Convert.js
+++ src/components/Convert.js
@@ -1,12 +1,22 @@
 import React, { useSate, useEffect } from 'react';
+import axios from 'axios';
 
 const Convert = ({ language, text }) => {
-
   useEffect(() => {
-    console.log('new lang or text');
+    axios.post(
+      'https://translation.googleapis.com/language/translate/v2',
+      {},
+      {
+        params: {
+          q: text,
+          target: language.value,
+          key: 'AIzaSyCHUCmpR7cT_yDFHC98CZJy2LTms-IwDlM',
+        },
+      }
+    );
   }, [language, text]);
 
-  return <div>Convert</div>
-}
+  return <div>Convert</div>;
+};
 
 export default Convert;
