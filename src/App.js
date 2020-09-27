6262edb5ae9ea3659760ede6001ea31a29127603
Martin Luna <uxmoon@gmail.com>
2020-09-27 12:11:35 -0300

    Translate widget - scaffolding  the component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import Accordion from "./components/Accordion";
 import Search from "./components/Search";
 import Dropdown from "./components/Dropdown";
+import Translate from './components/Translate';
 
 /* static array */
 const items = [
@@ -35,19 +36,9 @@ const options = [
 ];
 
 export default () => {
-  const [selected, setSelected] = useState(options[0]);
-  const [showDropdown, setShowDropdown] = useState(true);
-
   return (
     <div className="ui container">
-      <button className="ui button" onClick={() => setShowDropdown(!showDropdown)}>Toggle Dropdown</button>
-      {showDropdown ?
-          <Dropdown
-            selected={selected}
-            onSelectedChange={setSelected}
-            options={options}
-          /> : null
-      }
+      <Translate />
     </div>
   );
 };
